77fecea573b91ab53739609a3fabe580cec088e9
Rafał <rafass1000@gmail.com>
2022-01-09 13:57:19 +0100

    SSl

--- src/Actions/api.js
+++ src/Actions/api.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const config = require('../config.json');
 const app = express();
-
+const https = require('https');
+const fs = require('fs');
+https_options = {
+    key: fs.readFileSync("../../ssl/key.key"),
+    cert: fs.readFileSync("../../ssl/cert.crt")
+}
 app.get('/', (request, response) => {
     response.send("Cześć Świecie!");
 });
@@ -9,5 +14,5 @@ app.get('/', (request, response) => {
 app.use(express.static(config.API.ContentPath));
 
 module.exports = () => {
-    app.listen(config.API.Port);
+    https.createServer(https_options, app).listen(config.API.Port);
 }
