29b1dc5098f755e4534cf57befecfb91eeaa2c3b
Rafał <rafass1000@gmail.com>
2022-01-10 18:55:22 +0100

    Ssl removal

--- src/Actions/api.js
+++ src/Actions/api.js
@@ -3,10 +3,10 @@ const config = require("../config.json");
 const app = express();
 const https = require("https");
 const fs = require("fs");
-https_options = {
-  key: fs.readFileSync("../../ssl/key.key"),
-  cert: fs.readFileSync("../../ssl/cert.crt"),
-};
+// https_options = {
+//   key: fs.readFileSync("../../ssl/key.key"),
+//   cert: fs.readFileSync("../../ssl/cert.crt"),
+// };
 app.get("/", (request, response) => {
   response.send("Cześć Świecie!");
 });
